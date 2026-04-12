07be6e24e37551eed5f09c1fa5412d711cc62ccf
Rempest <thehuman130410@gmail.com>
2026-04-12 22:04:12 +0500

    Implement motion detection with contours and display
    Added motion detection functionality with contour analysis and visual feedback.

--- motion_detection.py
+++ motion_detection.py
@@ -13,5 +13,34 @@ while cam.isOpen():
     # Threshold (black and white image)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     # Enlarge motion areas
+    dilated = cv2.dilate(thresh, None, iterations=3)
+    # find the motion
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for comtours in contours:
+        # ignore the little motion
+        if cv2.contourArea(contour) < 1000:
+            continue
+        #show the window
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+        # red text
+        cv2.putText(frame1, "Movement detected", (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.imshow("Motion Detection", frame1)
+
+       
+       frame1 = frame2
+       ret, frame2 = cap.read()
+
+      #press "ESP" to exit
+        if cv2.waitKey(40) == 27:
+          break
+
+cam.release()
+cv2.destroyAllWindows()
+
+        
+    
 
 
