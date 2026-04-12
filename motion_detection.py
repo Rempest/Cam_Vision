f44c01a4364476c1b2c74d283aec5912eff84db9
Rempest <thehuman130410@gmail.com>
2026-04-12 21:49:23 +0500

    Refactor motion detection to use frame difference
    Updated camera index and modified motion detection logic to compare two frames for detecting motion.

--- motion_detection.py
+++ motion_detection.py
@@ -1,14 +1,17 @@
 import cv2
-cam = cv2.VideoCapture(1)
-while True:
-    rem, frame = cam.read()
-    print("The camera reads 1 frame")
-    if not rem:
-        break
-    color_cam = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("The picture", color_cam)
-    if cv2.waitKey(1) & 0xFF == ord("p"):
-       break
-cam.release()
-cv2.destroyAllWindows()
+cam = cv2.VideoCapture(0)
+#frames to find the difference
+frame1, rem1 = cam.read()
+frame2, rem2 = cam.read()
+while cam.isOpen():
+    #difference between the frames
+    diff = cv2.absdiff(frame1, frame2)
+    # Convert to gray
+    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    #blur
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # Threshold (black and white image)
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+    # Enlarge motion areas
+
 
